8bbf9a05bf16e11e850ba992154c4eebeb286b8f
GeorgePin <gonnaa1@gmail.com>
2022-08-21 14:26:51 +0300

    React example. First version, basic index page is ready.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/css/style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "Hello React 2 " };
+  }
+  render() {
+    return (
+      <section className="flex justify-center">
+        <div className="w-1/2">
+          <div className="text-center">
+            <div className="my-4">{this.state.title}</div>
+            <div>
+              <button className="p-1 bg-blue-700 tet-white my-2">
+                Toggle image
+              </button>
+              <img src="https://images.unsplash.com/photo-1543933720-b55c838bb109?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
